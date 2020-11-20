7ebf633d4fc42efc10873ed71e5b206bda134143
Rak Laptudirm <68542775+raklaptudirm@users.noreply.github.com>
2020-11-20 10:53:17 +0530

    Add files via upload

---
+++ PreciseLib.js
@@ -0,0 +1,53 @@
+let preAdd = (a, b) => {
+    if (a.length > b.length){
+        while (a.length > b.length){
+            b = "0" + b;
+        }
+    } else if (a.length < b.length){
+        while (a.length < b.length){
+            a = "0" + a;
+        }
+    }
+    var i; var ex = 0; var ans = ""; var add;
+    for (i = (a.length-1); i >= 0; i--){
+        add = parseInt(a[i]) + parseInt(b[i]) + ex;
+        ex = (add - (add % 10))/10;
+        add = add % 10;
+        ans = add + ans;
+    }
+    if (ex != 0) ans = ex + ans;
+    return ans;
+}
+
+let preMult = (a, b) => {
+    if (b > a) {
+        var c = a;
+        a = b;
+        b = c;
+    }
+    var i; var ans; var ex = 0; var mult; var j;
+    var values = [];
+    for (i = (b.length-1); i >= 0; i--){
+        ans = ""; ex = 0;
+        for (j = (a.length-1); j >= 0; j--){
+            mult = (parseInt(b[i]) * parseInt(a[j])) + ex;
+            ex = (mult - (mult % 10))/10;
+            mult = mult % 10;
+            ans = mult + ans;
+        }
+        values.push(ans);
+    }
+
+    ans = "0";
+
+    for (i = 0; i < values.length; i++){
+        for (j = 0; j < i; j++){
+            values[i] = values[i] + "0";
+        }
+        ans = preAdd(ans, values[i]);
+    }
+
+    return ans;
+}
+
+console.log(preMult("82345327592593258328538253553858328757325732784368366325", "328528358358219832993255343547847457886834283583285"));
