0ca378aaa372a9084e730fcbfa37ebb24f0ef73d
Rak Laptudirm <68542775+raklaptudirm@users.noreply.github.com>
2021-01-21 22:29:50 +0530

    Precise is now a module

---
+++ src/PreciseLib.js
@@ -0,0 +1,55 @@
+export class precise {
+    constructor () {
+        this.add = (a, b) => {
+            if (a.length > b.length){
+                while (a.length > b.length){
+                    b = "0" + b;
+                }
+            } else if (a.length < b.length){
+                while (a.length < b.length){
+                    a = "0" + a;
+                }
+            }
+            var i; var ex = 0; var ans = ""; var add;
+            for (i = (a.length-1); i >= 0; i--){
+                add = parseInt(a[i]) + parseInt(b[i]) + ex;
+                ex = (add - (add % 10))/10;
+                add = add % 10;
+                ans = add + ans;
+            }
+            if (ex != 0) ans = ex + ans;
+            return ans;
+        }
+
+        this.mult = (a, b) => {
+            if (b > a) {
+                var c = a;
+                a = b;
+                b = c;
+            }
+            var i; var ans; var ex = 0; var mult; var j;
+            var values = [];
+            for (i = (b.length-1); i >= 0; i--){
+                ans = ""; ex = 0;
+                for (j = (a.length-1); j >= 0; j--){
+                    mult = (parseInt(b[i]) * parseInt(a[j])) + ex;
+                    ex = (mult - (mult % 10))/10;
+                    mult = mult % 10;
+                    ans = mult + ans;
+                }
+                values.push(ans);
+            }
+
+            ans = "0";
+
+            for (i = 0; i < values.length; i++){
+                for (j = 0; j < i; j++){
+                    values[i] = values[i] + "0";
+                }
+                ans = this.add(ans, values[i]);
+            }
+
+            return ans;
+        }
+    }
+}
