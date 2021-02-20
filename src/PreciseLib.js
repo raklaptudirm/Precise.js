271024d4b56aa6fb182d2db69a756c1b1ab6488c
Rak Laptudirm <68542775+raklaptudirm@users.noreply.github.com>
2021-02-20 13:25:27 +0530

    static funcs

--- src/PreciseLib.js
+++ src/PreciseLib.js
@@ -1,55 +1,57 @@
-export class precise {
-    constructor () {
-        this.add = (a, b) => {
-            if (a.length > b.length){
-                while (a.length > b.length){
-                    b = "0" + b;
-                }
-            } else if (a.length < b.length){
-                while (a.length < b.length){
-                    a = "0" + a;
-                }
-            }
-            var i; var ex = 0; var ans = ""; var add;
-            for (i = (a.length-1); i >= 0; i--){
-                add = parseInt(a[i]) + parseInt(b[i]) + ex;
-                ex = (add - (add % 10))/10;
-                add = add % 10;
-                ans = add + ans;
-            }
-            if (ex != 0) ans = ex + ans;
-            return ans;
-        }
-
-        this.mult = (a, b) => {
-            if (b > a) {
-                var c = a;
-                a = b;
-                b = c;
-            }
-            var i; var ans; var ex = 0; var mult; var j;
-            var values = [];
-            for (i = (b.length-1); i >= 0; i--){
-                ans = ""; ex = 0;
-                for (j = (a.length-1); j >= 0; j--){
-                    mult = (parseInt(b[i]) * parseInt(a[j])) + ex;
-                    ex = (mult - (mult % 10))/10;
-                    mult = mult % 10;
-                    ans = mult + ans;
-                }
-                values.push(ans);
-            }
-
-            ans = "0";
-
-            for (i = 0; i < values.length; i++){
-                for (j = 0; j < i; j++){
-                    values[i] = values[i] + "0";
-                }
-                ans = this.add(ans, values[i]);
-            }
-
-            return ans;
-        }
+export class Precise {
+  static add (a, b) {
+    if (!(typeof a === 'string')) throw new Error(`Expected string, received ${typeof a}`)
+    else if (!(typeof b === 'string')) throw new Error(`Expected string, received ${typeof b}`)
+    if (a.length > b.length) {
+      while (a.length > b.length) {
+        b = '0' + b
+      }
+    } else if (a.length < b.length) {
+      while (a.length < b.length) {
+        a = '0' + a
+      }
     }
+    let i; let ex = 0; let ans = ''; let add
+    for (i = (a.length - 1); i >= 0; i--) {
+      add = parseInt(a[i]) + parseInt(b[i]) + ex
+      ex = (add - (add % 10)) / 10
+      add = add % 10
+      ans = add + ans
+    }
+    if (ex !== 0) ans = ex + ans
+    return ans
+  }
+
+  static mult (a, b) {
+    if (!(typeof a === 'string')) throw new Error(`Expected string, received ${typeof a}`)
+    else if (!(typeof b === 'string')) throw new Error(`Expected string, received ${typeof b}`)
+    if (b.length > a.length) {
+      const c = a
+      a = b
+      b = c
+    }
+    let i; let ans; let ex = 0; let mult; let j
+    const values = []
+    for (i = (b.length - 1); i >= 0; i--) {
+      ans = ''; ex = 0
+      for (j = (a.length - 1); j >= 0; j--) {
+        mult = (parseInt(b[i]) * parseInt(a[j])) + ex
+        ex = (mult - (mult % 10)) / 10
+        mult = mult % 10
+        ans = mult + ans
+      }
+      values.push(ans)
+    }
+
+    ans = '0'
+
+    for (i = 0; i < values.length; i++) {
+      for (j = 0; j < i; j++) {
+        values[i] = values[i] + '0'
+      }
+      ans = this.add(ans, values[i])
+    }
+
+    return ans
+  }
 }
---
+++ test.js
@@ -0,0 +1,5 @@
+import { Precise } from './src/PreciseLib.js'
+
+console.log(Precise.mult('73847578', '62748537'))
+
+// node test.js
